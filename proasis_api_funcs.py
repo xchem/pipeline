e0015766bb2ab0b52150045caf0239e3acfeb03e
Skyner <reskyner@googlemail.com>
2018-01-08 12:50:18 +0000

    function to delete all inhouse structures, with exceptions list of projects

--- proasis_api_funcs.py
+++ proasis_api_funcs.py
@@ -40,3 +40,28 @@ def get_strucids_from_project(project):
 def delete_structure(strucid):
     delete_string = str('/usr/local/Proasis2/utils/removestruc.py -s ' + str(strucid))
     os.system(delete_string)
+
+def delete_project(name):
+    delete_string = str('/usr/local/Proasis2/utils/removeoldproject.py -p ' + str(name))
+    os.system(delete_string)
+
+
+def delete_all_inhouse(exception_list=['Zitzmann', 'Ali', 'CMGC_Kinases']):
+    all_projects_url = 'http://cs04r-sc-vserv-137.diamond.ac.uk/proasisapi/v1.4/projects/'
+
+    json_string_projects = get_json(all_projects_url)
+    dict_projects = dict_from_string(json_string_projects)
+
+    all_projects = dict_projects['ALLPROJECTS']
+
+    for project in all_projects:
+        project_name = project['project']
+
+        for exception in exception_list:
+            if exception in project_name:
+                continue
+            else:
+                strucids = get_strucids_from_project(str(project_name))
+                for strucid in strucids:
+                    delete_structure(strucid)
+                delete_project(project_name)
