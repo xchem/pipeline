1d77218386b54eeecef9114f8220a896a2ac4fa4
Skyner <reskyner@googlemail.com>
2018-03-07 10:03:19 +0000

    classes to run vina

--- luigi_classes/prepare_dock.py
+++ luigi_classes/prepare_dock.py
@@ -2,6 +2,10 @@ import luigi
 from htmd.ui import *
 import os
 import subprocess
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit import Geometry
+from rdkit.Chem import rdMolTransforms
 
 class PrepProtein(luigi.Task):
     protein_pdb = luigi.Parameter()
@@ -12,13 +16,23 @@ class PrepProtein(luigi.Task):
         pass
 
     def output(self):
-        return luigi.LocalTarget(os.path.join(self.root_dir, self.docking_dir, str(self.protein_pdb).replace('.pdb', '_prepared.pdb')))
+        return luigi.LocalTarget(os.path.join(self.root_dir, self.docking_dir, str(self.protein_pdb).replace('.pdb', '_prepared.pdbqt')))
 
     def run(self):
         os.chdir(os.path.join(self.root_dir, self.docking_dir))
         mol = Molecule(self.protein_pdb)
         prot = proteinPrepare(mol)
-        prot.write(self.output().path)
+        prot.remove('resname WAT')
+        prot.write(str(self.protein_pdb).replace('.pdb', '_prepared.pdb'))
+
+        protein = os.path.join(self.root_dir, self.docking_dir, str(self.protein_pdb).replace('.pdb', '_prepared.pdb'))
+        command = ' '.join([
+            # self.ssh_command,
+            'obabel', protein, '-O', protein.replace('.pdb', '.pdbqt'), '-r'])
+        print(command)
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = process.communicate()
+
 
 class PrepLigand(luigi.Task):
     ligand_sdf = luigi.Parameter()
@@ -30,7 +44,7 @@ class PrepLigand(luigi.Task):
 
     def output(self):
         return luigi.LocalTarget(os.path.join(self.root_dir, self.docking_dir,
-                                              str(self.ligand_sdf).replace('.sdf', '.mol2')))
+                                              str(self.ligand_sdf).replace('.sdf', '_prepared.pdbqt')))
 
     def run(self):
         os.chdir(os.path.join(self.root_dir, self.docking_dir))
@@ -43,6 +57,90 @@ class PrepLigand(luigi.Task):
         print(command)
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = process.communicate()
+
+        # prepare pdbqt from mol2
+        ligand = os.path.join(self.root_dir, self.docking_dir, self.ligand_sdf.replace('.sdf', '.mol2'))
+        command = ' '.join([
+            # self.ssh_command,
+            'obabel', ligand, '-O', ligand.replace('.mol2', '_prepared.pdbqt')])
+        print(command)
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = process.communicate()
+
+
+class VinaDock(luigi.Task):
+    root_dir = luigi.Parameter()
+    docking_dir = luigi.Parameter(default='comp_chem')
+    ligand_sdf = luigi.Parameter()
+    receptor_pdb = luigi.Parameter()
+    vina_exe = luigi.Parameter(default='/dls_sw/apps/xchem/autodock_vina_1_1_2_linux_x86/bin/vina')
+    box_size = luigi.Parameter(default='[40, 40, 40]')
+
+    def requires(self):
+        return PrepLigand(root_dir=self.root_dir, ligand_sdf=self.ligand_sdf), \
+               PrepProtein(root_dir=self.root_dir, protein_pdb=self.receptor_pdb)
+
+    def output(self):
+        pass
+
+    def run(self):
+        ligand = os.path.join(self.root_dir, self.docking_dir, self.ligand_sdf.replace('.sdf', '.mol2'))
+
+        mol = Chem.MolFromMol2File(ligand)
+        conf = mol.GetConformer()
+        centre = rdMolTransforms.ComputeCentroid(conf)
+
+        box_size = eval(self.box_size)
+
+        os.chdir(os.path.join(self.root_dir, self.docking_dir))
+
+        outfile = os.path.join(self.root_dir, self.docking_dir, self.receptor_pdb.replace('.pdb', '_prepared2.pdbqt'))
+        if os.path.isfile(outfile):
+            os.remove(outfile)
+
+        with open(os.path.join(self.root_dir, self.docking_dir, self.receptor_pdb.replace('.pdb', '_prepared.pdbqt')), 'r') as infile:
+            for line in infile:
+                if 'ROOT' in line or 'BRANCH' in line or 'TORSDOF' in line:
+                    continue
+                else:
+                    with open(outfile, 'a') as f:
+                        f.write(line)
+
+        command = [
+            'cd',
+            os.path.join(self.root_dir, self.docking_dir),
+            ';',
+            self.vina_exe,
+            '--receptor',
+            os.path.join(self.root_dir, self.docking_dir, self.receptor_pdb.replace('.pdb', '_prepared2.pdbqt')),
+            '--ligand',
+            os.path.join(self.root_dir, self.docking_dir, self.ligand_sdf.replace('.sdf', '_prepared.pdbqt')),
+            '--center_x',
+            centre.x,
+            '--center_y',
+            centre.y,
+            '--center_z',
+            centre.z,
+            '--size_x',
+            str(box_size[0]),
+            '--size_y',
+            str(box_size[1]),
+            '--size_z',
+            str(box_size[2]),
+        ]
+
+        command = ' '.join(str(v) for v in command)
+
+        print(command)
+
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = process.communicate()
+
+        print(out)
+        print(err)
+
+
+
 
 
 
