e49ed18ba83dbfd4bf5cff17f0c84ecab6339424
Skyner <reskyner@googlemail.com>
2018-04-15 14:04:05 +0100

    some comments

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,8 +1,11 @@
 import luigi
 
 def run_luigi_worker(task):
+    # initialise a worker to run task
     w = luigi.worker.Worker()
+    # schedule task
     w.add(task)
+    # run task - returns True if job is run successfully by worker
     status = w.run()
 
     return status
