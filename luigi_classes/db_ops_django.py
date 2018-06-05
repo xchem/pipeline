ca4a8a47281d9e53f444f54fd55db49ca58d3bc6
reskyner <reskyner@googlemail.com>
2018-06-05 12:03:21 +0100

    stop printing crap

--- luigi_classes/db_ops_django.py
+++ luigi_classes/db_ops_django.py
@@ -641,8 +641,8 @@ class FindSearchPaths(luigi.Task):
         for path in list(set(search_paths)):
             count = search_paths.count(path)
             if count > 1:
-                print(path)
-                print([i for (x, i) in zipped if x == path])
+                # print(path)
+                # print([i for (x, i) in zipped if x == path])
 
                 while path in search_paths:
                     search_paths.remove(path)
@@ -650,9 +650,9 @@ class FindSearchPaths(luigi.Task):
                 to_exclude.append(path)
 
         for path, sdbfile in zipped:
-            print(path)
-            print(sdbfile)
-            print(os.path.join(path, sdbfile))
+            # print(path)
+            # print(sdbfile)
+            # print(os.path.join(path, sdbfile))
             yield AddPanddaTables(search_path=path, soak_db_filepath=self.soak_db_filepath,
                                   sdbfile=os.path.join(path, sdbfile))
 
