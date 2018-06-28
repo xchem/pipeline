97e2cd1a7290dce96fd16f52b906c45c9f75a22d
reskyner <reskyner@googlemail.com>
2018-06-28 15:03:25 +0100

    lab keys

--- run_check.py
+++ run_check.py
@@ -11,6 +11,7 @@ def check_table(model, results, translation):
             if translation[key] == 'CrystalName':
                 test_xchem_val = eval(str('lab_object[0].' + key + '.crystal_name'))
             if translation[key] == 'DimpleReferencePDB':
+                print(str('lab_object[0].' + key + '.reference_pdb'))
                 print(eval(str('lab_object[0].' + key + '.reference_pdb')))
             if soakdb_val == '' or soakdb_val == 'None':
                 continue
