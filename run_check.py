505b7b7d00fd310e2fe3f945bf53c96a4b1a0868
Skyner <reskyner@googlemail.com>
2018-06-28 12:42:18 +0100

    run_check

---
+++ run_check.py
@@ -0,0 +1,38 @@
+import setup_django
+from db.models import *
+from functions.db_functions import soakdb_query
+
+database_file = '/dls/labxchem/data/2017/lb17884-1/processing/database/soakDBDataFile.sqlite'
+print('Checking Database file ' + database_file)
+print('Running soakdb_query...')
+results = soakdb_query(database_file)
+print('Number of rows from file = ' + str(len(results)))
+
+if len(Crystal.objects.filter(file__filename=database_file)) == len(results):
+    status = True
+else:
+    status = False
+
+print('Checking same number of rows in test_xchem: ' + str(status))
+if not status:
+    print('FAIL: no of entries in test_xchem = ' + str(len(Crystal.objects.filter(file__filename=database_file))))
+
+proteins = list(set([protein for protein in [protein['ProteinName'] for protein in results]]))
+
+print('Unique targets in soakdb file: ' + str(proteins))
+
+#
+#
+# print(len(proteins))
+#
+# print(proteins)
+#
+# for row in results:
+#     # set up blank dictionary to hold model values
+#     d = {}
+#     # get the keys and values of the query
+#     row_keys = row.keys()
+#     row_values = list(tuple(row))
+#
+# # print())
+#
