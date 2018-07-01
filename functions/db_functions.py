a3f0430972a361cfd5aed7f7be4840809900e013
reskyner <reskyner@googlemail.com>
2018-07-01 17:31:14 +0100

    print translation dict

--- functions/db_functions.py
+++ functions/db_functions.py
@@ -177,8 +177,8 @@ def transfer_table(translate_dict, filename, model):
         for i, x in enumerate(row_keys):
             if x in dict((v, k) for k, v in translate_dict.items()).keys():
                 key = dict((v, k) for k, v in translate_dict.items())[x]
-
-
+                if key == 'res_high_15_sigma':
+                    print(key)
 
                 if key not in d.keys():
                     d[key] = ''
