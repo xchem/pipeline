15873e4e33ba99b387eb3e29736606b5d74e86f7
reskyner <reskyner@googlemail.com>
2018-07-06 16:05:31 +0100

    refactor

--- functions/db_functions.py
+++ functions/db_functions.py
@@ -216,7 +216,7 @@ def test_duplicate_method(filename):
 
             if unique == len(results):
                 for result in results:
-                    print(datetime.datetime.strftime(result['LastUpdated'], '%Y%m%d%H%M'))
+                    print(int(result['LastUpdated'].replace('-','').replace(':','').rstrip()))
                 print('/n')
 
 
