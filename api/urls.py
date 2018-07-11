aff62b6c2ba8b35006ba772b40e06879fd8ebec0
reskyner <reskyner@googlemail.com>
2018-07-11 15:18:48 +0100

    print in wsgi

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,14 @@
+from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views as drf_views
 import db.views as db_views
 
 
 router = DefaultRouter()
 # Register the basic data
 router.register(r"target", db_views.TargetView)
+
+urlpatterns = [
+    url(r"^", include(router.urls)),
+    url(r"^auth$", drf_views.obtain_auth_token, name="auth"),
+]
