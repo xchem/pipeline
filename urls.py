606e3c3dea015dd51c68c48af52955874a1b77a3
Skyner <reskyner@googlemail.com>
2018-07-12 11:20:50 +0100

    add comma for filter_fields

--- urls.py
+++ urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path(r'admin/', admin.site.urls),
-    path(r'target/', include("db.urls")),
+    path(r'target/', include("api.urls")),
 ]
