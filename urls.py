bfaca80cae59960b5db406c632f1d28e04f649af
Anthony Bradley <anthony.richard.bradley@gmail.com>
2018-07-12 16:18:18 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
+from django.conf.urls import include, url
 
 urlpatterns = [
-    path(r'admin/', admin.site.urls),
-    path(r"api/", include("api.urls")),
+    url(r"^admin/", admin.site.urls),
+    url(r"^api/", include("api.urls")),
 ]
