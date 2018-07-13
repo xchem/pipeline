8036536ff1332f56cb9150a45741529d6905cda2
reskyner <reskyner@googlemail.com>
2018-07-13 15:51:34 +0100

    test app

--- overview/views.py
+++ overview/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.template import loader
 
 def crystals_from_target(request, target):
-    crystals = ', '.join([crystal.crystal_name for crystal in Crystal.objects.filter(target__target_name=target)])
+    crystals = Crystal.objects.filter(target__target_name=target)
     template = loader.get_template('overview/index.html')
     context = {
         'crystals': crystals,
