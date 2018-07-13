dbd788071a5d970b21378628ee0517dcbc724c7c
reskyner <reskyner@googlemail.com>
2018-07-13 15:38:17 +0100

    test app

--- overview/views.py
+++ overview/views.py
@@ -5,7 +5,7 @@ from django.template import loader
 
 def crystals_from_target(request, target):
     crystals = ', '.join([crystal.crystal_name for crystal in Crystal.objects.filter(target__target_name=target)])
-    template = loader.get_template('overview/index.html')
+    template = loader.get_template('templates/overview/index.html')
     context = {
         'crystals': crystals,
     }
