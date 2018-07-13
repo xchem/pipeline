ee1fec2681e598f2119b54c9bbbde0db73c86486
reskyner <reskyner@googlemail.com>
2018-07-13 16:48:32 +0100

    targets

--- overview/urls.py
+++ overview/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:target>/', views.crystals_from_target, name='crystals')
+    path(r'targets/', views.targets, name='crystals')
 ]
--- overview/views.py
+++ overview/views.py
@@ -1,12 +1,12 @@
-from db.models import Crystal
+from db.models import Target
 
 from django.http import HttpResponse
 from django.template import loader
 
-def crystals_from_target(request, target):
-    crystals = Crystal.objects.filter(target__target_name=target)
+def targets(request):
+    targets = Target.objects.all()
     template = loader.get_template('overview/index.html')
     context = {
-        'crystals': crystals,
+        'targets': targets,
     }
     return HttpResponse(template.render(context, request))
