195f4c56cbf8c8448a51820f70995f00d5ddbc56
reskyner <reskyner@googlemail.com>
2018-07-13 15:27:05 +0100

    test app

--- urls.py
+++ urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import include, url
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
     url(r"^api/", include("api.urls")),
+    url(r"^overview/", include("overview.urls"))
 ]
