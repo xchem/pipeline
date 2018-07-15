28a235b3d712934e8830d4b43850e92e8999e734
reskyner <reskyner@googlemail.com>
2018-07-15 17:56:27 +0100

    debug

--- luigi_classes/transfer_pandda.py
+++ luigi_classes/transfer_pandda.py
@@ -291,6 +291,9 @@ class AddPanddaData(luigi.Task):
         return luigi.LocalTarget(os.path.join(self.search_path, 'transfer_pandda_data.done'))
 
     def requires(self):
+        
+        os.remove(self.output().path)
+
         if not os.path.isfile(FindPanddaInfo(search_path=self.search_path, soak_db_filepath=self.soak_db_filepath,
                               sdbfile=self.sdbfile).output().path):
             return FindPanddaInfo(search_path=self.search_path, soak_db_filepath=self.soak_db_filepath,
