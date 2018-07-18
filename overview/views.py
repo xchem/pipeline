769bcff736d25aae6e220538317973cce9ec73c5
reskyner <reskyner@googlemail.com>
2018-07-18 12:23:35 +0100

    start to add ajax

--- overview/views.py
+++ overview/views.py
@@ -18,9 +18,9 @@ def targets(request):
 def get_graph(request):
     queryset = Target.objects.filter()
     filter_fields = ('target_name',)
-    
-    submission = request.query_params.get('submission', None)
-    crystals = Crystal.objects.filter(target__target_name=submission)
-    data = {'number': len(crystals)}
 
-    return JsonResponse(data)
+    # submission = request.query_params.get('submission', None)
+    # crystals = Crystal.objects.filter(target__target_name=submission)
+    # data = {'number': len(crystals)}
+
+    return JsonResponse(request)
