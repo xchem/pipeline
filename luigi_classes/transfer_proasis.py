c8ff3e23c6c33eca84f3a631657a17877c519232
reskyner <reskyner@googlemail.com>
2018-07-20 13:45:59 +0100

    date

--- luigi_classes/transfer_proasis.py
+++ luigi_classes/transfer_proasis.py
@@ -51,8 +51,9 @@ class InitDBEntries(luigi.Task):
 
             try:
                 dimple = Dimple.objects.get(crystal_name=obj.crystal_name)
-                if dimple.reference.reference_pdb:
-                    proasis_lead_entry = ProasisLeads.objects.get_or_create(reference_pdb=dimple.reference)
+                if dimple.reference:
+                    if dimple.reference.reference_pdb:
+                        proasis_lead_entry = ProasisLeads.objects.get_or_create(reference_pdb=dimple.reference)
 
             except:
                 print(traceback.format_exc())
