7d5cedb2415bd5a013f6ca21ef2430a5500509f4
reskyner <reskyner@googlemail.com>
2018-07-20 11:50:36 +0100

    date

--- luigi_classes/transfer_proasis.py
+++ luigi_classes/transfer_proasis.py
@@ -36,7 +36,7 @@ class InitDBEntries(luigi.Task):
                 break
 
             mod_date = misc_functions.get_mod_date(obj.bound_conf)
-            proasis_entry = ProasisHits.objects.get_or_create(refinement=obj, crystal=obj.crystal_name,
+            proasis_entry = ProasisHits.objects.get_or_create(refinement=obj, crystal_name=obj.crystal_name,
                                                       pdb_file=obj.bound_conf, modification_date=mod_date,
                                                       mtz=mtz[1], two_fofc=two_fofc[1], fofc=fofc[1])
             proasis_entry.save()
