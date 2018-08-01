59042c9f899e0898f359d609491bb735a1e7ceb7
reskyner <reskyner@googlemail.com>
2018-08-01 11:00:37 +0100

    interaction json

--- luigi_classes/transfer_proasis.py
+++ luigi_classes/transfer_proasis.py
@@ -241,14 +241,14 @@ class AddLead(luigi.Task):
         res_list = (list(set(res_list)))
         print(res_list)
         colon = ' :'
-        if len(str(res_list[0])) >= 4:
+        if len(str(res_list[0])) >= 10:
             colon = ':'
         lig1 = str("'" + str(res_list[0]) + colon)
-        if len(str(res_list[1])) >= 4:
+        if len(str(res_list[1])) >= 10:
             colon = ':'
         lig1 += str(str(res_list[1]) + colon)
         close = " ' "
-        if len(str(res_list[2])) >= 4:
+        if len(str(res_list[2])) >= 10:
             close = "' "
         lig1 += str(str(res_list[2]) + close)
 
