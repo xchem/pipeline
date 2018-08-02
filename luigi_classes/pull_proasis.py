ec7c9db059555e5ff0a23f09e0a2422dda4bcb18
reskyner <reskyner@googlemail.com>
2018-08-02 13:34:54 +0100

    antechamber

--- luigi_classes/pull_proasis.py
+++ luigi_classes/pull_proasis.py
@@ -207,36 +207,36 @@ class CreateMolTwoFile(luigi.Task):
             out = out.decode('ascii')
             if err:
                 err = err.decode('ascii')
+                raise Exception(err)
 
             print(out)
             print(err)
-
             o.mol2 = outfile.split('/')[-1]
             o.save()
 
 
 
-class CreateHMol(luigi.Task):
-    hit_directory = luigi.Parameter(default='/dls/science/groups/proasis/LabXChem/')
-    crystal_id = luigi.Parameter()
-    refinement_id = luigi.Parameter()
-
-    def requires(self):
-        return CreateMolFile(
-            hit_directory=self.hit_directory, crystal_id=self.crystal_id, refinement_id=self.refinement_id)
-
-    def output(self):
-        proasis_hit = ProasisHits.objects.get(crystal_name_id=self.crystal_id, refinement_id=self.refinement_id)
-        crystal_name = proasis_hit.crystal_name.crystal_name
-        target_name = proasis_hit.crystal_name.target.target_name
-        return luigi.LocalTarget(os.path.join(
-            self.hit_directory, target_name, crystal_name, str(crystal_name + '_h.mol')))
-
-    def run(self):
-        mol_file = self.input().path
-        rd_mol = Chem.MolFromMolFile(mol_file, removeHs=False)
-        h_rd_mol = AllChem.AddHs(rd_mol, addCoords=True)
-        Chem.MolToMolFile(h_rd_mol, self.output().path)
+# class CreateHMol(luigi.Task):
+#     hit_directory = luigi.Parameter(default='/dls/science/groups/proasis/LabXChem/')
+#     crystal_id = luigi.Parameter()
+#     refinement_id = luigi.Parameter()
+#
+#     def requires(self):
+#         return CreateMolFile(
+#             hit_directory=self.hit_directory, crystal_id=self.crystal_id, refinement_id=self.refinement_id)
+#
+#     def output(self):
+#         proasis_hit = ProasisHits.objects.get(crystal_name_id=self.crystal_id, refinement_id=self.refinement_id)
+#         crystal_name = proasis_hit.crystal_name.crystal_name
+#         target_name = proasis_hit.crystal_name.target.target_name
+#         return luigi.LocalTarget(os.path.join(
+#             self.hit_directory, target_name, crystal_name, str(crystal_name + '_h.mol')))
+#
+#     def run(self):
+#         mol_file = self.input().path
+#         rd_mol = Chem.MolFromMolFile(mol_file, removeHs=False)
+#         h_rd_mol = AllChem.AddHs(rd_mol, addCoords=True)
+#         Chem.MolToMolFile(h_rd_mol, self.output().path)
 
 
 class CreateNoBufAltLocs(luigi.Task):
@@ -257,25 +257,31 @@ class GetInteractionJSON(luigi.Task):
     refinement_id = luigi.Parameter()
 
     def requires(self):
-        pass
-            # CreateMolFile(
-            # hit_directory=self.hit_directory, crystal_id=self.crystal_id, refinement_id=self.refinement_id)
+        return CreateMolFile(
+            hit_directory=self.hit_directory, crystal_id=self.crystal_id, refinement_id=self.refinement_id)
 
     def output(self):
-        proasis_hit = ProasisHits.objects.get(crystal_name_id=self.crystal_id, refinement_id=self.refinement_id)
-        crystal_name = proasis_hit.crystal_name.crystal_name
-        target_name = proasis_hit.crystal_name.target.target_name
-        return luigi.LocalTarget(os.path.join(
-            self.hit_directory, target_name, crystal_name, str(crystal_name + '_contacts.json')))
+        proasis_out = ProasisOut.objects.filter(proasis=ProasisHits.objects.get(crystal_name_id=self.crystal_id,
+                                                                                refinement_id=self.refinement_id))
+        ligs = [o.ligand for o in proasis_out]
+        root = [o.root for o in proasis_out]
+        start = [o.start for o in proasis_out]
+        return [luigi.LocalTarget(os.path.join(r, s, str(s + '_' + l.replace(' ', '') + '_contacts.json')))
+                for (r, s, l) in zip(root, start, ligs)]
 
     def run(self):
-        proasis_hit = ProasisHits.objects.get(crystal_name_id=self.crystal_id, refinement_id=self.refinement_id)
-        strucid = proasis_hit.strucid
-
-        url = str("http://cs04r-sc-vserv-137.diamond.ac.uk/proasisapi/v1.4/sc/" + str(strucid) +
-                  "?strucsource=inh&plain=1")
-
-        json_string_scorpion = proasis_api_funcs.get_json(url)
-
-        with open(self.output().path, 'w') as outfile:
-            json.dump(json_string_scorpion, outfile, ensure_ascii=False)
+        proasis_out = ProasisOut.objects.filter(proasis=ProasisHits.objects.get(crystal_name_id=self.crystal_id,
+                                                                                refinement_id=self.refinement_id))
+        for o in proasis_out:
+            lig = o.ligand
+            strucid = o.proasis.strucid
+            root = o.root
+            start = o.start
+            outfile = os.path.join(root, start, str(start + '_' + lig.replace(' ', '') + '_contacts.json'))
+            out = proasis_api_funcs.get_lig_sdf(strucid, lig, outfile)
+            if out:
+                o.contacts = out.split('/')[-1]
+            else:
+                raise Exception('contacts json not produced!')
+            o.save()
+            
