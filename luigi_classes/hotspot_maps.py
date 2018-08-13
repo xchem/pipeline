1f2457e7c41b0df16fa8d223506723cdcdc7a64e
reskyner <reskyner@googlemail.com>
2018-08-13 15:37:24 +0100

    run hotspot map calcs class

--- luigi_classes/hotspot_maps.py
+++ luigi_classes/hotspot_maps.py
@@ -61,10 +61,10 @@ class WriteRunCheckHot(luigi.Task):
 
         return [WriteHotJob(apo_pdb=a, directory=d) for (a, d) in zip(apo_pdb, directory)], \
                [cluster_submission.SubmitJob(job_directory='/'.join(j.split('/')[:-1]),
-                                             job_script=j.split('/')[-1]) for j in w_output_paths], \
-               [cluster_submission.CheckJob(
-                   output_files=[j.replace('_apo_hotspots.sh', '_acceptor.ccp4'),
-                                 j.replace('_apo_hotspots.sh', '_donor.ccp4'),
-                                 j.replace('_apo_hotspots.sh', '_apolar.ccp4')],
-                   job_file=j.split('/')[-1],
-                   directory='/'.join(j.split('/')[:-1])) for j in w_output_paths]
+                                             job_script=j.split('/')[-1]) for j in w_output_paths] #, \
+               # [cluster_submission.CheckJob(
+               #     output_files=[j.replace('_apo_hotspots.sh', '_acceptor.ccp4'),
+               #                   j.replace('_apo_hotspots.sh', '_donor.ccp4'),
+               #                   j.replace('_apo_hotspots.sh', '_apolar.ccp4')],
+               #     job_file=j.split('/')[-1],
+               #     directory='/'.join(j.split('/')[:-1])) for j in w_output_paths]
