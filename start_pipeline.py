7f9bb1a307cc2fcc79e9be986411a99953996d55
reskyner <reskyner@googlemail.com>
2018-08-28 19:23:16 +0100

    add getoutfiles back into pipeline

--- start_pipeline.py
+++ start_pipeline.py
@@ -31,7 +31,7 @@ class StartPipeline(luigi.WrapperTask):
         yield InitDBEntries(date=self.date, hit_directory=self.hit_directory)
         yield UploadLeads(date=self.date, hit_directory=self.hit_directory)
         yield WriteBlackLists(date=self.date, hit_directory=self.hit_directory)
-        # yield GetOutFiles(date=self.date, hit_directory=self.hit_directory)
+        yield GetOutFiles(date=self.date, hit_directory=self.hit_directory)
         # yield WriteRunCheckHot()
         # yield UpdateOtherFields()
 
