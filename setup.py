fc1f552c645d940224b4c2d9c1aa927437f4519e
abradle <you@example.com>
2018-08-28 13:44:38 +0100

    Update models to use mysql

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.0.1-alpha-5",
+    version="0.0.1-alpha-6",
     description="Library for XChem Django database",
     long_description=long_description,
     # The project's main homepage.
