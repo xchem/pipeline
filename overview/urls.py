99ee660319cc648d228489dfe371680debbc65f8
reskyner <reskyner@googlemail.com>
2018-09-25 18:00:13 +0100

    add modification date checks to status overview

--- overview/urls.py
+++ overview/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path(r'targets/', views.targets, name='targets'),
     path(r'targets/graph/', views.get_graph, name='get_graph'),
-    path(r'targets/mod_date/', views.get_mod_date, name='get_mod_date')
+    path(r'targets/mod_date/', views.get_mod_date, name='get_mod_date'),
 ]
