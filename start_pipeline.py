45d71c018b5d2f5a1288a776a53cfa0edad7b6cf
reskyner <reskyner@googlemail.com>
2018-09-26 16:03:45 +0100

    get out files

--- start_pipeline.py
+++ start_pipeline.py
@@ -5,7 +5,8 @@ setup_django()
 import luigi
 import datetime
 from luigi_classes.transfer_pandda import TransferPandda
-from luigi_classes.transfer_proasis import InitDBEntries, UploadLeads, WriteBlackLists, UploadHits, UpdateOtherFields
+from luigi_classes.transfer_proasis import InitDBEntries, UploadLeads, WriteBlackLists, UploadHits
+from luigi_classes.pull_proasis import GetOutFiles
 from luigi_classes.transfer_soakdb import StartTransfers
 from luigi_classes.transfer_verne import UpdateVerne
 
@@ -34,6 +35,7 @@ class StartPipeline(luigi.WrapperTask):
         yield TransferPandda(date_time=self.date_time, soak_db_filepath=self.soak_db_filepath)
         yield InitDBEntries(date=self.date, hit_directory=self.hit_directory)
         yield UploadLeads(date=self.date, hit_directory=self.hit_directory)
+        yield GetOutFiles()
         yield WriteBlackLists(date=self.date, hit_directory=self.hit_directory)
         yield UpdateVerne()
 
