5502f897d0bee4881aaad8dca6cfc3b890fb7d81
reskyner <reskyner@googlemail.com>
2018-10-12 14:48:01 +0100

    change activate line in bash script

--- run_services.sh
+++ run_services.sh
@@ -3,9 +3,8 @@ source ~/.bashrc
 source activate pipeline
 initdb db_files
 nohup luigid >/dev/null 2>&1 &
-pg_ctl -D db_files -l logfile start
+pg_ctl -D db_files -l logfile start -A
 createdb test_xchem
-cd pipeline
 python manage.py makemigrations db
 python manage.py migrate db
 
