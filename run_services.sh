668e88033c219ff0ad5b928078d29a82530e61d8
reskyner <reskyner@googlemail.com>
2018-10-12 15:01:30 +0100

    change bash script

--- run_services.sh
+++ run_services.sh
@@ -5,6 +5,6 @@ initdb db_files
 nohup luigid >/dev/null 2>&1 &
 pg_ctl -D db_files -l logfile start
 createdb test_xchem
-python manage.py makemigrations db
-python manage.py migrate db
+python manage.py makemigrations xchem_db
+python manage.py migrate xchem_db
 
