7c8396438ae497834c1066a42461f98f4e33b917
Skyner <reskyner@googlemail.com>
2018-10-21 12:24:19 +0100

    add example datafile and json + creation script

---
+++ tests/data/soakdb_files/make_json.py
@@ -0,0 +1,24 @@
+import sqlite3
+import json
+ 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+ 
+connection = sqlite3.connect("soakDBDataFile.sqlite")
+connection.row_factory = dict_factory
+ 
+cursor = connection.cursor()
+ 
+cursor.execute("select * from mainTable")
+ 
+# fetch all or one we'll go for all.
+ 
+results = cursor.fetchall()
+ 
+with open('soakDBDataFile.json', 'w') as f:
+    json.dump(results, f)
+ 
+connection.close()
