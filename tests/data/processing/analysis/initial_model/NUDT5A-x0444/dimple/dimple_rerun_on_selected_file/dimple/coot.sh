9e3cecdbb2d2442140697ee3295d0e94c9244fc5
Skyner <reskyner@googlemail.com>
2018-10-21 12:31:24 +0100

    sample 2

---
+++ tests/data/processing/analysis/initial_model/NUDT5A-x0444/dimple/dimple_rerun_on_selected_file/dimple/coot.sh
@@ -0,0 +1,1 @@
+/dls/science/groups/i04-1/software/pandda-update/ccp4/ccp4-7.0/bin/coot --no-guano /dls/labxchem/data/2018/lb18145-71/processing/analysis/initial_model/NUDT5A-x0444/dimple/dimple_rerun_on_selected_file/dimple/blob1-coot.py
