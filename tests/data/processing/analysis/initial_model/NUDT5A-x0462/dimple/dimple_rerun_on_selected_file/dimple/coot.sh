fdaec82d1716f8b4bd2206855984ae6f25bcb8cf
Skyner <reskyner@googlemail.com>
2018-10-21 13:21:43 +0100

    reduce test data

--- tests/data/processing/analysis/initial_model/NUDT5A-x0462/dimple/dimple_rerun_on_selected_file/dimple/coot.sh
+++
@@ -1,1 +0,0 @@
-/dls/science/groups/i04-1/software/pandda-update/ccp4/ccp4-7.0/bin/coot --no-guano /dls/labxchem/data/2018/lb18145-71/processing/analysis/initial_model/NUDT5A-x0462/dimple/dimple_rerun_on_selected_file/dimple/blob1-coot.py
