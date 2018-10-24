b94f752ec21a2d242bd69acad0d5b823dcd11710
reskyner <reskyner@googlemail.com>
2018-10-24 19:07:06 +0100

    add file

--- tests/test_pandda_upload.py
+++ tests/test_pandda_upload.py
@@ -57,7 +57,7 @@ class TestFindLogs(unittest.TestCase):
         self.assertTrue(sorted(file_list) == sorted(globbed))
 
     def test_get_files_from_log_function(self):
-        log_file = '/pipeline/tests/data/processing/analysis/panddas/logs/pandda-2018-07-30-2352.log'
+        log_file = '/pipeline/tests/data/processing/analysis/panddas/logs/pandda-2018-07-29-1940.log'
         pver, input_dir, output_dir, sites_file, events_file, Error = pf.get_files_from_log(log_file)
         print(pver)
         print(input_dir)
