a9e449dcb7444bcaf072c5211ca32ea0f9fcd56b
reskyner <reskyner@googlemail.com>
2018-10-24 18:20:24 +0100

    allow database to not be in search path (use processing instead)

--- tests/test_pandda_upload.py
+++ tests/test_pandda_upload.py
@@ -44,10 +44,11 @@ class TestFindLogs(unittest.TestCase):
                                                       date_time=self.date.strftime("%Y%m%d%H")))
 
         self.assertTrue(find_paths)
+        self.assertTrue(
+            '0,/pipeline/tests/data/database/soakDBDataFile.sqlite,/pipeline/tests/data/,/pipeline/tests/data/processing/soakDBDataFile.sqlite'
+            in open(FindSearchPaths(soak_db_filepath=os.path.join(self.sdb_filepath, self.db_file_name),
+                                    date_time=self.date.strftime("%Y%m%d%H")).output().path, 'r').read())
 
-        with open(FindSearchPaths(soak_db_filepath=os.path.join(self.sdb_filepath, self.db_file_name),
-                                  date_time=self.date.strftime("%Y%m%d%H")).output().path, 'r') as f:
-            print(f.read())
 
 
     # def test_find_pandda_logs(self):
