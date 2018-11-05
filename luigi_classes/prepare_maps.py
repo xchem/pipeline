f26924f9c5a0bd9acabc1296bb0590551a9b815e
reskyner <reskyner@googlemail.com>
2018-11-05 14:47:55 +0000

    start mask

--- luigi_classes/prepare_maps.py
+++ luigi_classes/prepare_maps.py
@@ -1,18 +1,21 @@
 import luigi
+import os
 
 
 class CutOutEvent(luigi.Task):
     ssh_command = 'ssh uzw12877@nx.diamond.ac.uk'
+    directory = luigi.Parameter()
     mapin = luigi.Parameter()
     mapout = luigi.Parameter()
     xyzin = luigi.Parameter()
 
 
     def run(self):
-        mapmask = '''mapmask mapin %s mapout %s xyzin %s << eof
+        mapmask = '''module load ccp4 && mapmask mapin %s mapout %s xyzin %s << eof
             border 12
             end
         eof
         ''' % (self.mapin, self.mapout, self.xyzin)
 
+        os.system(str(self.ssh_command + ' && ' + 'cd ' + self.directory + ' && ' + mapmask))
         print(mapmask)
