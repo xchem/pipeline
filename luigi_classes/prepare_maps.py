54e9cd105b2967ee72e85297c53ca6fcc7f0d270
reskyner <reskyner@googlemail.com>
2018-11-05 14:32:02 +0000

    start mask

---
+++ luigi_classes/prepare_maps.py
@@ -0,0 +1,18 @@
+import luigi
+
+
+class CutOutEvent(luigi.Task):
+    ssh_command = 'ssh uzw12877@nx.diamond.ac.uk'
+    mapin = luigi.Parameter()
+    mapout = luigi.Parameter()
+    xyzin = luigi.Parameter()
+
+
+    def run(self):
+        mapmask = '''mapmask mapin %s mapout %s xyzin %s << eof
+            border 12
+            end
+        eof
+        ''' .format(self.mapin, self.mapout, self.xyzin)
+
+        print(mapmask)
