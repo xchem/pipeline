4d6256aada037ff612478723d806e9e3134bba09
reskyner <reskyner@googlemail.com>
2018-11-05 16:27:11 +0000

    start mask

--- luigi_classes/prepare_maps.py
+++ luigi_classes/prepare_maps.py
@@ -1,12 +1,13 @@
 import luigi
 import os
 import subprocess
+import openbabel
 
 
 class CutOutEvent(luigi.Task):
     ssh_command = 'ssh uzw12877@nx.diamond.ac.uk'
     directory = luigi.Parameter()
-    mapin = luigi.Parameter()
+    mol_file = luigi.Parameter()
     mapout = luigi.Parameter()
     xyzin = luigi.Parameter()
     border = luigi.Parameter(default='6')
@@ -19,15 +20,26 @@ class CutOutEvent(luigi.Task):
 
 
     def run(self):
+        # convert to pdbqt with obabel
+        obConv = openbabel.OBConversion()
+        obConv.SetInAndOutFormats('mol', 'pdb')
+        mol = openbabel.OBMol()
+
+        # read pdb and write pdbqt
+        obConv.ReadFile(mol, self.mol_file)
+        obConv.WriteFile(mol, self.mol_file.replace('.mol', '.pdb'))
+
         mapmask = '''module load ccp4 && mapmask mapin %s mapout %s xyzin %s << eof
             border %s
             end
         eof
-        ''' % (self.mapin, self.mapout, self.xyzin, str(self.border))
+        ''' % (self.mol_file.replace('.mol', '.pdb'), self.mapout, self.xyzin, str(self.border))
 
         process = subprocess.Popen(str(self.ssh_command + ' "' + 'cd ' + self.directory + ';' + mapmask + '"'),
                                    shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
         out, err = process.communicate()
+
+        os.remove(os.path.join(self.directory, self.mol_file.replace('.mol', '.pdb')))
 
         if '(mapmask) - normal termination' not in out:
             raise Exception('mapmask failed!')
