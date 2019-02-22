2fce623da039132aaac5e2629a628cc9ba5a3098
reskyner <reskyner@googlemail.com>
2019-02-22 12:01:57 +0000

    add get current refinement to event model

--- xchem_db/serializers.py
+++ xchem_db/serializers.py
@@ -373,7 +373,7 @@ class FragspectCrystalSerializer(serializers.ModelSerializer):
     confidence = serializers.CharField(source='ligand_confidence')
     # crystal_resolution = serializers.SerializerMethodField()
     smiles = serializers.CharField(source='crystal.compound.smiles')
-    spacegroup = serializers.CharField(source='current_refinement.spacegroup')
+    spacegroup = serializers.CharField(source='current_refinement[0].spacegroup')
     # cell = serializers.SerializerMethodField()
     # cell_angles = serializers.SerializerMethodField()
     event_comment = serializers.CharField(source='comment')
