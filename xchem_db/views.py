c6a385917897d4d392b5ae2e6728f756bb39ee4d
reskyner <reskyner@googlemail.com>
2019-02-22 12:28:27 +0000

    add all fields from refinement and dataproc

--- xchem_db/views.py
+++ xchem_db/views.py
@@ -218,7 +218,7 @@ class ProasisOutView(viewsets.ReadOnlyModelViewSet):
 
 
 class FragspectCrystalView(viewsets.ReadOnlyModelViewSet):
-    queryset = PanddaEvent.objects.filter()
+    queryset = PanddaEvent.objects.filter().prefetch_related('crystal', 'site')
     serializer_class = FragspectCrystalSerializer
     filter_fields = ('crystal__target__target_name',)
 
