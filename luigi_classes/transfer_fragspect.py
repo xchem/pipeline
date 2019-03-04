d46757440ecda638fc23dfa742a28e40e92230c4
reskyner <reskyner@googlemail.com>
2019-03-04 14:05:44 +0000

    tmp_dir

--- luigi_classes/transfer_fragspect.py
+++ luigi_classes/transfer_fragspect.py
@@ -203,9 +203,9 @@ class KickOffFragspect(UpdateVerne):
                      PanddaEvent.objects.filter(
                          modified_date__lte=django.utils.timezone.now()).distinct('crystal__target__target_name')]
 
-        print(os.path.join(os.getcwd(),self.target_list))
+        print(os.path.join(os.getcwd(),self.target_list_file))
 
-        with open(os.path.join(os.getcwd(),self.target_list), 'w') as f:
+        with open(os.path.join(os.getcwd(),self.target_list_file), 'w') as f:
             f.write('\n'.join(to_upload))
 
         return StartFragspectLoader(username=self.username,
