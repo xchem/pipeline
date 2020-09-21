b15ace017704362982941f68afa7315bad2d4b1b
TJGorrie <tyler.gorrie-stone@diamond.ac.uk>
2020-09-21 14:41:37 +0100

    Remove the Save thing? is it a red herring?

--- functions/db_functions.py
+++ functions/db_functions.py
@@ -316,15 +316,14 @@ def transfer_table(translate_dict, filename, model):
 
         try:
             # This is janky...
-            print(f'Finding Entry for {crystal_name}')
             crys_obj = models.Crystal.objects.get(
                 target=target_obj,
                 crystal_name=crystal_name,
-                visit=visit_obj)
-            crys_obj.product = product_smiles
-            crys_obj.compound = compound_obj
+                visit=visit_obj,
+                product=product_smiles,
+                compound=compound_obj
+            )
         except models.Crystal.DoesNotExist:
-            print(f'...Does not exist... Creating Entry for: {crystal_name}')
             crys_obj = models.Crystal.objects.create(
                 target=target_obj,
                 crystal_name=crystal_name,
@@ -333,9 +332,6 @@ def transfer_table(translate_dict, filename, model):
                 compound=compound_obj
             )
 
-
-        print(crys_obj)
-        crys_obj.save()
         # now see if there's already a row for this crystal in the model we're currently using
         if model != models.Crystal:
             model_row, model_row_created = model.objects.get_or_create(crystal_name=crys_obj)
