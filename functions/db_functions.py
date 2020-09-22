10a9eb46c434eec18e4c005f1109a4d27175d002
Tyler Gorrie-Stone <tyler.gorrie-stone@diamond.ac.uk>
2020-09-22 09:23:39 +0100

    Adding Target back into get_or_create..
    Back to where we started...

--- functions/db_functions.py
+++ functions/db_functions.py
@@ -303,7 +303,7 @@ def transfer_table(translate_dict, filename, model):
         visit_obj = models.SoakdbFiles.objects.get(filename=filename)
         # put everything together and get the crystal object
         crys_obj, crys_obj_created = models.Crystal.objects.get_or_create(
-            # target=target_obj,
+            target=target_obj,
             crystal_name=crystal_name,
             visit=visit_obj,
             product=product_smiles,
