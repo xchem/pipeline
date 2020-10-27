dc39f9cd79f49488789b55916c6387a35890c9c4
TJGorrie <tyler.gorrie-stone@diamond.ac.uk>
2020-10-27 14:33:47 +0000

    Removing ZombieCode

--- luigi_classes/prepare_fragalysis.py
+++ luigi_classes/prepare_fragalysis.py
@@ -128,15 +128,7 @@ class CreateSymbolicLinks(luigi.Task):
                     print(mapmask)
                     proc = subprocess.run(mapmask, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
                                           executable='/bin/bash')
-                    #process = subprocess.Popen(
-                    #    str(self.ssh_command + ' "' + mapmask + '"'),
-                    #    shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-                    #out, err = process.communicate()
-                    #if '(mapmask) - normal termination' not in out:
-                    #    raise Exception('mapmask failed!')
-                    #os.symlink(fofc[0], fofc_pth)
-                    #os.system(
-                    #    f'module load ccp4 && mapmask mapin {fofc[0]} mapout {fofc_pth} xyzin {self.output().path} << eof\n border 6\n end\n eof')
+
                 if len(fofc2) > 0:
                     mapmask = '''module load ccp4 && mapmask mapin %s mapout %s xyzin %s << eof
                         border %s
@@ -146,22 +138,12 @@ class CreateSymbolicLinks(luigi.Task):
                     print(mapmask)
                     proc = subprocess.run(mapmask, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
                                           executable='/bin/bash')
-                    #process = subprocess.Popen(
-                    #    str(self.ssh_command + ' "' + mapmask + '"'),
-                    #    shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-                    #out, err = process.communicate()
-                    #if '(mapmask) - normal termination' not in out:
-                    #    raise Exception('mapmask failed!')
-                    #os.symlink(fofc2[0], fofc2_pth)
-                    #os.system(
-                    #    f'module load ccp4 && mapmask mapin {fofc2[0]} mapout {fofc2_pth} xyzin {self.output().path} << eof\n border 6\n end\n eof')
 
                 # probably should use enumerate
                 if len(event_maps) > 0:
                     event_num = 0
                     for i in event_maps:
                         fn = self.output().path.replace('.pdb', f'_event_{event_num}.ccp4')
-                        #os.symlink(i, fn)
                         mapmask = '''module load ccp4 && mapmask mapin %s mapout %s xyzin %s << eof
                             border %s
                             end
@@ -170,14 +152,6 @@ class CreateSymbolicLinks(luigi.Task):
                         print(mapmask)
                         proc = subprocess.run(mapmask, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
                                               executable='/bin/bash')
-                        #process = subprocess.Popen(
-                        #    str(self.ssh_command + ' "' + mapmask + '"'),
-                        #    shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-                        #out, err = process.communicate()
-                        #if '(mapmask) - normal termination' not in out:
-                        #    raise Exception('mapmask failed!')
-                        #os.system(
-                        #    f'module load ccp4 && mapmask mapin {i} mapout {fn} xyzin {self.output().path} << eof\n border 6\n end\n eof')
                         event_num += 1
 
                 if self.prod_smiles:
@@ -291,140 +265,3 @@ class CutMaps(luigi.Task):
 
         with self.output().open('w') as f:
             f.write('')
-
-# class AlignTargetXChem(AlignTarget):
-#    filter_by = Refinement.objects.filter(outcome__gte=4).filter(outcome__lte=6)
-#    resources = {'django': 1}
-#    date = luigi.Parameter(default=datetime.datetime.now())
-#    soak_db_filepath = luigi.Parameter(default=SoakDBConfig().default_path)
-#    hit_directory = luigi.Parameter(default=DirectoriesConfig().hit_directory)
-#    uuid = str(uuid.uuid4())
-#    input_dir = luigi.Parameter(default=DirectoriesConfig().fraginput_directory)
-#    output_dir = luigi.Parameter(default=DirectoriesConfig().fragstaging_directory)
-
-#    def requires(self):
-#        return BatchSymlinkBoundPDB(filter_by=self.filter_by,
-#                                    resources=self.resources,
-#                                    date=self.date,
-#                                    soak_db_filepath=self.soak_db_filepath,
-#                                    hit_directory=self.hit_directory(),
-#                                    uuid=self.uuid)
-
-
-# class ProcessAlignedPDBXChem(ProcessAlignedPDB):
-#    def requires(self):
-#        return AlignTargetXChem(input_dir=self.input_dir, output_dir=self.output_dir)
-
-
-# class BatchProcessAlignedPDBXChem(BatchProcessAlignedPDB):
-#    def requires(self):
-#        self.aligned_list = outlist_from_align(self.input_dir, self.output_dir)
-#        return [
-#            ProcessAlignedPDBXChem(
-#                target_name=self.target_name, input_file=i, output_dir=self.output_dir,
-#                input_dir=self.input_dir
-#            )
-#            for i in self.aligned_list
-#        ]
-
-# class BatchConvertAlignedXChem(BatchConvertAligned):
-#    filter_by = Refinement.objects.filter(outcome__gte=4).filter(outcome__lte=6)
-#    resources = {'django': 1}
-#    date = luigi.Parameter(default=datetime.datetime.now())
-#    soak_db_filepath = luigi.Parameter(default=SoakDBConfig().default_path)
-#    hit_directory = luigi.Parameter(default=DirectoriesConfig().hit_directory)
-#    uuid = str(uuid.uuid4())
-
-#    search_directory = luigi.Parameter(default=DirectoriesConfig().hit_directory)
-#    output_directory = luigi.Parameter(default=DirectoriesConfig().staging_directory)
-#    def requires(self):
-#        if os.path.isfile(self.output().path):
-#            os.remove(self.output().path)
-#        in_lst = [os.path.abspath(f.path) for f in os.scandir(self.search_directory) if f.is_dir()]
-#        out_lst = []
-#        target_names = []
-
-#        for f in in_lst:
-#            out = os.path.join(os.path.abspath(self.output_directory), f.split('/')[-1])
-#            out_lst.append(out)
-#            target_names.append(f.split('/')[-1])
-
-#        return [BatchProcessAlignedPDBXChem(input_dir=i, output_dir=self.output_directory, target_name=t)
-#                for (i, t) in list(zip(in_lst, target_names))]
-
-
-# class SymlinkBoundPDB(luigi.Task):
-#    crystal = luigi.Parameter()
-#    hit_directory = luigi.Parameter(default=DirectoriesConfig().hit_directory)
-#    soak_db_filepath = luigi.Parameter(default=SoakDBConfig().default_path)
-#    date = luigi.Parameter(default=datetime.datetime.now())
-#    smiles = luigi.Parameter(default=None)
-#    prod_smiles = luigi.Parameter(default=None)
-
-#    def requires(self):
-#        # return StartTransfers()
-#        return None
-
-
-#    def output(self):
-#        pth = os.path.join(self.hit_directory,
-#                           self.crystal.crystal_name.target.target_name,
-#                          str(self.crystal.crystal_name.crystal_name + '.pdb'))
-#        return luigi.LocalTarget(pth)
-
-#    def run(self):
-#        try:
-#            if not os.path.exists(os.readlink(self.output().path)):
-#                os.unlink(self.output().path)
-#        except FileNotFoundError:
-#            pass
-#        if not os.path.isdir('/'.join(self.output().path.split('/')[:-1])):
-#            os.makedirs('/'.join(self.output().path.split('/')[:-1]))
-#        file_obj = RefinementObjectFiles(refinement_object=self.crystal)
-#        file_obj.find_bound_file()
-#        if file_obj.bound_conf:
-#            try:
-
-#                os.symlink(file_obj.bound_conf, self.output().path)
-#                if self.prod_smiles:
-#                    smi = self.prod_smiles
-#                elif self.smiles:
-#                    smi = self.smiles
-#                 if self.smiles:
-#                smi_pth = self.output().path.replace('.pdb', '_smiles.txt')
-#                with open(smi_pth, 'w') as f:
-#                    f.write(str(smi))
-#                f.close()
-
-#            except:
-#                raise Exception(file_obj.bound_conf)
-#        else:
-#            self.crystal.outcome = 3
-#            self.crystal.save()
-
-
-# class BatchSymlinkBoundPDB(luigi.Task):
-#    filter_by = Refinement.objects.filter(outcome__gte=4).filter(outcome__lte=6)
-#    resources = {'django': 1}
-#    date = luigi.Parameter(default=datetime.datetime.now())
-#    soak_db_filepath = luigi.Parameter(default=SoakDBConfig().default_path)
-#    hit_directory = luigi.Parameter(default=DirectoriesConfig().hit_directory)
-#    uuid = str(uuid.uuid4())
-
-#    def requires(self):
-#        return [
-#            SymlinkBoundPDB(
-#                crystal=crystal,
-#                smiles=crystal.crystal_name.compound.smiles,
-#                prod_smiles=crystal.crystal_name.product,
-#                hit_directory=self.hit_directory,
-#                soak_db_filepath=self.soak_db_filepath
-#            )
-#            for crystal in self.filter_by
-#        ]
-
-#    def output(self):
-#        return DjangoTaskTarget(class_name=self.__class__.__name__, uuid=self.uuid)
-
-#    def run(self):
-#        Tasks.objects.create(task_name=self.__class__.__name__, uuid=self.uuid)
