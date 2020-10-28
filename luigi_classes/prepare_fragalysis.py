b15eb1847d911969da2bb60288e38b7d73120c9b
TJGorrie <tyler.gorrie-stone@diamond.ac.uk>
2020-10-28 08:59:34 +0000

    Only run alignment IF target is not in staging directory!

--- luigi_classes/prepare_fragalysis.py
+++ luigi_classes/prepare_fragalysis.py
@@ -181,7 +181,8 @@ class BatchAlignTargets(luigi.Task):
     def requires(self):
         targets = [target[0] for target in os.walk(self.input_directory)]
         print(targets)
-        return [AlignTarget(target=target) for target in targets]
+        # Run Align Target target name is NOT in staging directory...
+        return [AlignTarget(target=target) for target in targets if not os.path.exists(os.path.join(self.input_directory, target))]
 
     def output(self):
         return luigi.LocalTarget(os.path.join(DirectoriesConfig().log_directory,
