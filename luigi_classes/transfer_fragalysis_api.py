1a9d5d2970a922fc6ba6f8cef4141f1c02d82f5d
Tyler Gorrie-Stone <tyler.gorrie-stone@diamond.ac.uk>
2021-11-15 14:57:31 +0000

    stop luigi from crying...?

--- luigi_classes/transfer_fragalysis_api.py
+++ luigi_classes/transfer_fragalysis_api.py
@@ -32,8 +32,6 @@ class BatchTranslateFragalysisAPIOutput(luigi.Task):
         targs = [x for x in folders_containing_mols if compare_mod_date(glob.glob(os.path.join(x, '*.mol'))[0])]
         if len(targs) > 0:
             return [TranslateFragalysisAPIOutput(target=x) for x in targs]
-        else:
-            return 2  # idk...
 
     def output(self):
         return luigi.LocalTarget(os.path.join(DirectoriesConfig().log_directory,
