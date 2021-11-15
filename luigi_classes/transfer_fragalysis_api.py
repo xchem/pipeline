663f0bc8bf2a19a0b69abe3cb95ec914f60f8ff8
Tyler Gorrie-Stone <tyler.gorrie-stone@diamond.ac.uk>
2021-11-15 13:58:58 +0000

    d

--- luigi_classes/transfer_fragalysis_api.py
+++ luigi_classes/transfer_fragalysis_api.py
@@ -28,9 +28,9 @@ class BatchTranslateFragalysisAPIOutput(luigi.Task):
     def requires(self):
         # Honestly do not know how slow this is haha...
         staging_folders = [x[0] for x in os.walk(self.staging_directory) if 'aligned' in x[0]]
-        folders_containing_mols = [x for x in staging_folders if len(glob.glob(os.path.join(x, '*.mol'))) > 0]
+        folders_containing_mols = [x for x in staging_folders if len(glob.glob(os.path.join(x, '*.mol'))) > 0][0:2]
         # Check Modification date to fire off!!
-        return [TranslateFragalysisAPIOutput(target=x) for x in folders_containing_mols[0] if compare_mod_date(glob.glob(os.path.join(x, '*.mol'))[0])]
+        return [TranslateFragalysisAPIOutput(target=x) for x in folders_containing_mols if compare_mod_date(glob.glob(os.path.join(x, '*.mol'))[0])]
 
     def output(self):
         return luigi.LocalTarget(os.path.join(DirectoriesConfig().log_directory,
