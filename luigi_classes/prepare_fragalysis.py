6b524d80bc6aaed85cf0fa299d6c6b7c00c79c87
Tyler Gorrie-Stone <tyler.gorrie-stone@diamond.ac.uk>
2021-11-16 15:42:33 +0000

    stop reprocessing unchanged files

--- luigi_classes/prepare_fragalysis.py
+++ luigi_classes/prepare_fragalysis.py
@@ -41,18 +41,22 @@ class BatchRunCreateInputFiles(luigi.Task):
     # Should only be run if updated...
     def requires(self):
         # Holy crap.
-        return[
+        return [
             CreateInputFiles(
                 crystal=crystal,
-                smiles=[Compounds.objects.get(id=x['compound_id']).smiles for x in crystal.crystal_name.crystalcompoundpairs_set.values()],
+                smiles=[Compounds.objects.get(id=x['compound_id']).smiles for x in
+                        crystal.crystal_name.crystalcompoundpairs_set.values()],
                 prod_smiles=[x['product_smiles'] for x in crystal.crystal_name.crystalcompoundpairs_set.values()],
                 hit_directory=self.hit_directory,
                 soak_db_filepath=crystal.crystal_name.visit.filename,
                 monomer=crystal.crystal_name.target.pl_monomeric,
-                outpath=os.path.join(self.input_directory, crystal.crystal_name.target.target_name, str(crystal.crystal_name.crystal_name + '.pdb'))
+                outpath=os.path.join(self.input_directory, crystal.crystal_name.target.target_name,
+                                     str(crystal.crystal_name.crystal_name + '.pdb'))
             ) for crystal in self.filter_by if misc_functions.compare_dates_to_action(
                 sdb_mod=crystal.crystal_name.visit.modification_date,  # Is this the right thing to do???
-                pdb_out=misc_functions.get_mod_date(os.path.join(self.input_directory, crystal.crystal_name.target.target_name, str(crystal.crystal_name.crystal_name + '.pdb')))
+                pdb_out=misc_functions.get_mod_date(
+                    os.path.join(self.input_directory, crystal.crystal_name.target.target_name,
+                                 str(crystal.crystal_name.crystal_name + '.pdb')))
             )
         ]
 
@@ -85,18 +89,12 @@ class CreateInputFiles(luigi.Task):
 
     def output(self):
         # Change this to create a log entry? # Is it not running because of this?
-        return luigi.LocalTarget(os.path.join(DirectoriesConfig().log_directory, str('symboliclinks/transfers_' + str(self.crystal.crystal_name.crystal_name) + str(self.date) + '.done')))
+        return luigi.LocalTarget(os.path.join(DirectoriesConfig().log_directory, str(
+            'symboliclinks/transfers_' + str(self.crystal.crystal_name.crystal_name) + str(self.date) + '.done')))
 
     def run(self):
         # This shouldn't be scheduled unless soakdbfile has been updated...
         outpath = self.outpath
-
-        try:
-            #if not os.path.exists(os.readlink(outpath)):
-            #    os.unlink(outpath)
-            os.unlink(outpath)
-        except FileNotFoundError:
-            pass
 
         if not os.path.isdir('/'.join(outpath.split('/')[:-1])):
             os.makedirs('/'.join(outpath.split('/')[:-1]))
@@ -113,19 +111,30 @@ class CreateInputFiles(luigi.Task):
                 if self.crystal.pdb_latest:
                     input_pdb = self.crystal.pdb_latest
                 else:
-                    input_pdb = ''
+                    return ''  # Exit out of the runtime...
             except:
-                input_pdb = ''
-        if input_pdb is '':
+                return ''  # Exit out of the function...
+
+        # ...
+        if os.path(exists):
+            old = get_mod_date(get_filepath_of_potential_symlink(outpath))
+            new = get_mod_date(input_pdb)
+            process = int(new) > int(old)
+        else:
+            process = True
+        if process:
+            os.unlink(outpath)
+        if not process:
             cutmaps = False  # Do Not cut maps.
         else:
             if self.monomer:
                 os.symlink(input_pdb, outpath)
             else:
                 command = f'/dls/science/groups/i04-1/fragprep/scripts/biomol.sh {input_pdb} {outpath}'
-                proc = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, executable='/bin/bash')
+                proc = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+                                      executable='/bin/bash')
             # This needs to be zipped up.
-            if self.smiles is not '': # should be a [''] maybe who knows..
+            if self.smiles is not '':  # should be a [''] maybe who knows..
                 smi = '\n'.join([s if p in ['None', None, ''] else p for p, s in zip(self.prod_smiles, self.smiles)])
                 smi_pth = outpath.replace('.pdb', '_smiles.txt')
                 with open(smi_pth, 'w') as f:
@@ -147,7 +156,8 @@ class CreateInputFiles(luigi.Task):
                     end
                 eof
                 ''' % (fofc[0], fofc_pth, outpath, str(0))
-                proc = subprocess.run(mapmask, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, executable='/bin/bash')
+                proc = subprocess.run(mapmask, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+                                      executable='/bin/bash')
 
             if len(fofc2) > 0:
                 mapmask = '''module load ccp4 && mapmask mapin %s mapout %s xyzin %s << eof
@@ -155,7 +165,8 @@ class CreateInputFiles(luigi.Task):
                     end
                 eof
                 ''' % (fofc2[0], fofc2_pth, outpath, str(0))
-                proc = subprocess.run(mapmask, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, executable='/bin/bash')
+                proc = subprocess.run(mapmask, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+                                      executable='/bin/bash')
 
             if len(event_maps) > 0:
                 event_num = 0
@@ -166,198 +177,9 @@ class CreateInputFiles(luigi.Task):
                         end
                     eof
                     ''' % (i, fn, outpath, str(0))
-                    proc = subprocess.run(mapmask, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, executable='/bin/bash')
+                    proc = subprocess.run(mapmask, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+                                          executable='/bin/bash')
                     event_num += 1
-
-        with self.output().open('w') as f:
-            f.write('')
-
-
-class BatchAlignTargets(luigi.Task):
-    log_directory = luigi.Parameter(default=DirectoriesConfig().log_directory)
-    staging_directory = luigi.Parameter(default=DirectoriesConfig().staging_directory)
-    input_directory = luigi.Parameter(default=DirectoriesConfig().input_directory)
-    date = luigi.Parameter(default=datetime.datetime.now())
-
-    def requires(self):
-        # Check list of targets that have staging dirs
-        targets = [target[0] for target in os.walk(self.input_directory)]
-        # Decide which mode to run.
-        return [DecideAlignTarget(target=target) for target in targets]
-
-    def output(self):
-        return luigi.LocalTarget(os.path.join(DirectoriesConfig().log_directory,
-                                              str('Alignment/aligned_' + str(self.date) + '.done')))
-
-    def run(self):
-        with self.output().open('w') as f:
-            f.write('')
-
-
-class DecideAlignTarget(luigi.Task):
-    # Target is /inputdir/targetname
-    target = luigi.Parameter()
-    staging_directory = luigi.Parameter(default=DirectoriesConfig().staging_directory)
-    input_directory = luigi.Parameter(default=DirectoriesConfig().input_directory)
-    log_directory = luigi.Parameter(default=DirectoriesConfig().log_directory)
-    date = luigi.DateParameter(default=datetime.datetime.now())
-
-    def requires(self):
-        base = os.path.basename(self.target)
-        staging_dir = os.path.join(self.staging_directory, base)
-        if not os.path.exists(staging_dir):
-            # If staging direct with name does not exist do a big alignment
-            return AlignTarget(target=self.target)
-        else:
-            # If the folder exists:
-            # Check if the file exists
-            # If the file exists, check the date of the file to whats in the db!
-            aligned_dir = os.path.join(staging_dir, 'aligned')
-            infile = glob.glob(os.path.join(self.target, '*.pdb'))
-            infile_bases = [os.path.basename(x).replace('.pdb', '') for x in infile]
-            staging_files = glob.glob(os.path.join(aligned_dir, '*', '*.mol'))
-            staging_bases = [os.path.basename(x).rsplit('_', 1)[0] for x in staging_files]
-            not_aligned = list(set(infile_bases) - set(staging_bases))
-            check_for_updates = list(set(infile_bases) - set(not_aligned))
-            updated = []
-            for i in check_for_updates:
-                infile_date = [get_mod_date(x) for x in infile if f'{i}.pdb' in x]
-                if len(infile_date) == 1:
-                    infile_date = infile_date[0]
-                else:
-                    raise Exception('Multiple input pdbs with the same name? Somehow?')
-                staging_dates = [get_mod_date(get_filepath_of_potential_symlink(x)) for x in staging_files if f'{i}' in x]
-                diffs = [int(infile_date) > int(y) for y in staging_dates]
-                print(infile_date)
-                print(staging_dates)
-                print(diffs)
-                if any(diffs):
-                    updated.append(i)
-
-            to_align = list(set(not_aligned).union(set(updated)))
-            return [AlignTargetToReference(target=os.path.join(self.input_directory, base, f'{f}.pdb')) for f in to_align]
-
-    def output(self):
-        target_name = self.target.rsplit('/', 1)[1]
-        return luigi.LocalTarget(os.path.join(DirectoriesConfig().log_directory,
-                                              f'Alignment/Decide_aligned_{target_name}' + str(self.date) + '.done'))
-
-    def run(self):
-        with self.output().open('w') as f:
-            f.write('')
-
-
-class AlignTarget(luigi.Task):
-    # Target is /inputdir/targetname
-    target = luigi.Parameter()
-    staging_directory = luigi.Parameter(default=DirectoriesConfig().staging_directory)
-    log_directory = luigi.Parameter(default=DirectoriesConfig().log_directory)
-    date = luigi.DateParameter(default=datetime.datetime.now())
-
-    def requires(self):
-        return None
-
-    def output(self):
-        target_name = self.target.rsplit('/', 1)[1]
-        return luigi.LocalTarget(os.path.join(DirectoriesConfig().log_directory,
-                                              f'Alignment/aligned_{target_name}' + str(self.date) + '.done'))
-
-    def run(self):
-        target_name = self.target.rsplit('/', 1)[1]
-        # This is NOT the way to do this Tyler. But I am a noob at python so it'll work...
-        os.system(f'/dls/science/groups/i04-1/software/miniconda_3/envs/fragalysis_env2/bin/python /dls/science/groups/i04-1/software/tyler/fragalysis-api/fragalysis_api/xcimporter/xcimporter.py --in_dir={self.target} --out_dir={self.staging_directory} --target {target_name} -m -c')
-        with self.output().open('w') as f:
-            f.write('')
-
-class AlignTargetToReference(luigi.Task):
-    worker_timeout = 600
-    retry_count = 1
-    # Target is /inputdir/targetname.pdb
-    target = luigi.Parameter()
-    staging_directory = luigi.Parameter(default=DirectoriesConfig().staging_directory)
-    log_directory = luigi.Parameter(default=DirectoriesConfig().log_directory)
-    date = luigi.DateParameter(default=datetime.datetime.now())
-
-    def requires(self):
-        return None
-
-    def output(self):
-        target_name = os.path.basename(self.target).replace('.pdb', '')
-        return luigi.LocalTarget(os.path.join(DirectoriesConfig().log_directory,
-                                              f'Alignment/aligned_{target_name}' + str(self.date) + '.done'))
-
-    # Clean up
-    def run(self):
-        target_name = os.path.dirname(self.target).rsplit('/', 1)[1]
-        # Clean-up tmp and mono folders if previous tasks fail?
-        tmpfolder = os.path.join(self.staging_directory, f'tmp{target_name}')
-        monofolder = os.path.join(self.staging_directory, f'mono{target_name}')
-        if os.path.exists(tmpfolder) and os.path.isdir(tmpfolder):
-            shutil.rmtree(tmpfolder)
-        if os.path.exists(monofolder) and os.path.isdir(monofolder):
-            shutil.rmtree(monofolder)
-        # This is NOT the way to do this Tyler. But I am a noob at python so it'll work...
-        command = f'/dls/science/groups/i04-1/software/miniconda_3/envs/fragalysis_env2/bin/python /dls/science/groups/i04-1/software/tyler/fragalysis-api/fragalysis_api/xcimporter/single_import.py --in_file={self.target} --out_dir={self.staging_directory} --target {target_name} -m -c'
-        subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
-                       executable='/bin/bash')
-        with self.output().open('w') as f:
-            f.write('')
-
-
-class BatchCutMaps(luigi.Task):
-    log_directory = luigi.Parameter(default=DirectoriesConfig().log_directory)
-    staging_directory = luigi.Parameter(default=DirectoriesConfig().staging_directory)
-    input_directory = luigi.Parameter(default=DirectoriesConfig().input_directory)
-    date = luigi.Parameter(default=datetime.datetime.now())
-
-    def requires(self):
-        stagingfolders = glob.glob(os.path.join(self.staging_directory, '*'))
-        stagingfolders = [x for x in stagingfolders if 'tmp' not in x]
-        stagingfolders = [x for x in stagingfolders if 'mono' not in x]
-        return [CutMaps(target=target) for target in stagingfolders]
-
-    def output(self):
-        return luigi.LocalTarget(os.path.join(DirectoriesConfig().log_directory,
-                                              str('Cutting/cut_' + str(self.date) + '.done')))
-
-    def run(self):
-        with self.output().open('w') as f:
-            f.write('')
-
-
-class CutMaps(luigi.Task):
-    target = luigi.Parameter()
-    staging_directory = luigi.Parameter(default=DirectoriesConfig().staging_directory)
-    log_directory = luigi.Parameter(default=DirectoriesConfig().log_directory)
-    date = luigi.DateParameter(default=datetime.datetime.now())
-
-    def requires(self):
-        return None
-
-    def output(self):
-        target_name = os.path.basename(self.target)
-        return luigi.LocalTarget(os.path.join(DirectoriesConfig().log_directory,
-                                              f'Cutting/cut_{target_name}' + '.done'))
-
-    def run(self):
-        target_name = os.path.basename(self.target)
-        targs = glob.glob(os.path.join(self.staging_directory, target_name, 'aligned', '*'))
-        targs = [x for x in targs if 'pdb_file_failures' not in x]
-
-        for i in targs:
-            crys = os.path.basename(i)
-            maps = glob.glob(os.path.join(i, '*.map')) + glob.glob(os.path.join(i, '*.ccp4'))
-            for j in maps:
-                fn = j
-                mapmask = '''module load ccp4 && mapmask mapin %s mapout %s xyzin %s << eof
-                    border %s
-                    end
-                eof
-                ''' % (fn, fn, os.path.join(i, f"{crys}.pdb"), str(12))
-                print(mapmask)
-                proc = subprocess.run(mapmask, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
-                                      executable='/bin/bash')
-                #os.system(f'module load ccp4 && mapmask mapin {j} mapout {j} xyzin {os.path.join(i, f"{crys}.pdb")} << eof\n border 6\n end\n eof')
 
         with self.output().open('w') as f:
             f.write('')
