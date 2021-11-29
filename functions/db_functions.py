177a5e84d28cfaa7c733eda9330abaef4e5e08d6
Tyler Gorrie-Stone <tyler.gorrie-stone@diamond.ac.uk>
2021-11-29 11:32:57 +0000

    debugging

--- functions/db_functions.py
+++ functions/db_functions.py
@@ -444,6 +444,7 @@ def transfer_table(translate_dict, filename, model):
                     )
                     if len(filter_set) == 0:  # this is new!
                         print(d[key].crystal_name)
+                        print(filename)
                         crys_obj = models.Crystal.objects.get(crystal_name=d[key], visit=models.SoakdbFiles.objects.get(filename=filename))
                         crys_obj.compound.add(compound_obj)
                         crys_obj.save()
